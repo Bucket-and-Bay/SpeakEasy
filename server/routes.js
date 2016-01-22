87454a4afa3c024d2c2f935608717b15deb38283
jason3j <jason3j@gmail.com>
2016-01-22 15:51:43 -0700

    added first server files

---
+++ server/routes.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var videoLoader = require('./analysis/videoLoader.js');
+var videoAnalyzer = require('./analysis/videoAnalyzer.js');
+var router = express.Router();
+
+// eventually use Auth
---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json({type: 'application/*+json'}));
+
+
+///api/analyze
+
+app.use(express.static(__dirname + './../client'));
+app.listen(port, function () {
+  console.log('Listening on port: ', port);
+});
