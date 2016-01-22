e6a78024775f9a719d8d371544d538c68d64672a
tiff8433 <tiffany.l.huang@gmail.com>
2016-01-22 12:36:33 -0800

    Initial file config and react setup

---
+++ client/config/routes.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Main = require('../components/Main.js');
+var Router = require('react-router');
+var Route = Router.Route;
+
+
+module.exports =  (
+  <Route path='/' component={Main}> 
+  
+  </Route>
+
+)
