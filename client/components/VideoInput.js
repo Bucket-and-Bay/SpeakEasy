e9316fcf24dddb5a435334be54a2886f263fe12c
tiff8433 <tiffany.l.huang@gmail.com>
2016-01-23 14:43:33 -0800

    uploads file to streamable; shortcode logged

--- client/components/VideoInput.js
+++ client/components/VideoInput.js
@@ -1,15 +1,23 @@
 var React = require('react');
+var axios = require('axios');
 
 var VideoInput = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
   },
   handleFile: function() {
-    console.log(this.refs.video.files[0]);
+  var video = this.refs.video.files[0];    
+  var formData = new FormData();
+  formData.append('file', video);
+  axios.post('https://api.streamable.com/upload', formData)
+    .then(function(response){
+      //shortcode to send to server
+      console.log(response.data.shortcode, 'line 15');
+    })
   },
   render: function() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} encType="multipart/form-data">
         <div className="file-field input-field">  
           <div className="btn">
             <span>File</span>
