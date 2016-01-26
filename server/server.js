4ad3cc68f0ef85108cf460bd7aa34723642ee992
tiff8433 <tiffany.l.huang@gmail.com>
2016-01-25 16:17:02 -0800

    Adds CORS heading

--- server/server.js
+++ server/server.js
@@ -14,10 +14,15 @@ var port = process.env.PORT || 3000;
 // app.use(bodyParser.json({type: 'application/*+json'}));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 ///api/analyze
 
-app.use(express.static(__dirname + './../client'));
+app.use(express.static(__dirname + './../client/public'));
 app.listen(port, function () {
   console.log('Listening on port: ', port);
 });
