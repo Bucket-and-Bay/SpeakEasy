88a6ba1c0e940936d7012e28baafe52e851301dc
Allan Trinh <daigortrinh@gmail.com>
2016-01-26 13:50:04 -0800

    user login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,20 +1,21 @@
 var User = require('../models/user.model.js')
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
 
 var createUser = function(req, res){
   var user = {
     username: req.body.username,
     password: req.body.password
   }
-  User.findOne(user)
-    .then(function(user){
-      if(user){
+  User.findOne({username: user.username})
+    .then(function(exists){
+      if(exists){
         res.sendStatus(409)
       } else {
-        User(user).save()
+        new User(user).save()
           .then(function(user){
             //successful signup
-            console.log(user);
-            res.send(201)
+            res.sendStatus(201)
           })
           .catch(function(err){
             throw err;
@@ -28,18 +29,18 @@ var login = function(req, res){
     username: req.body.username,
     password: req.body.password
   }
+  console.log(user, 'line 32')
   User.findOne(user)
-      .then(function(user){
-        if(user){
-          // set cookies?
-          res.sendStatus(200); // login successful
-        } else {
-          res.sendStatus(401) // incorrect credentialss
-        }
-      })
-      .catch(function(err){
-        throw err;
-      })
+    .then(function(exists){
+      if(exists){
+        console.log('liine 36 200 success')
+        // set cookies?
+        res.sendStatus(200); // login successful
+      } else {
+        console.log('line 40 401 failure login')
+        res.sendStatus(401) // incorrect credentialss
+      }
+    })
 };
 
 module.exports = {
