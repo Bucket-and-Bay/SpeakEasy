16e88915a51bd0b8ded7fab95032027269a7a6f6
Zan Rosenthal <zan.rosenthal@gmail.com>
2016-01-25 20:30:23 -0500

    Video post route now saves analysis to database, and sends a request response to client without waiting on analysis to finish.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -6,15 +6,7 @@ var userSchema = new mongoose.Schema({
   password      : String,
   firstName     : String,
   lastName      : String,
-  videoAnalyses : [{
-    videoID:{
-      type: ObjectId,
-       ref: 'Analysis'
-     },
-      thumbnailUrl : String,
-      date         : {type: Date, default: Date.now},
-      title        : String
-    }]
+  analyses : [{type: ObjectId,ref: 'Analysis'}]
 });
 
 module.exports = mongoose.model('User', UserSchema);
