e5707e4e88249ae7013e2ae1b1ad24aa6979ff8f
Allan Trinh <daigortrinh@gmail.com>
2016-01-26 16:29:44 -0800

    combined package.jsons

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: "./app/App.js",
+  entry: "./client/app/App.js",
   output: {
-    filename: 'public/bundle.js'
+    filename: 'client/public/bundle.js'
   },
   module: {
     loaders: [
