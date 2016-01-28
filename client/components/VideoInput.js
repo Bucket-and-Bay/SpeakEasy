2fd89ac3e3008fc7c28e2eecf6ac471d4d148248
Allan Trinh <daigortrinh@gmail.com>
2016-01-28 00:29:59 -0800

    added submit button and file input on line

--- client/components/VideoInput.js
+++ client/components/VideoInput.js
@@ -3,27 +3,35 @@ var helpers = require('../config/helper.js');
 
 var VideoInput = React.createClass({
   handleSubmit: function(e) {
-    console.log('submit');
+
     e.preventDefault();
+
+    if(this.refs.video.files.length > 0){
+      var video = this.refs.video.files[0];
+      var validVideoFormats = {
+        'video/mp4': true,
+        'video/quicktime': true,
+        'video/avi': true
+      };
+      if (validVideoFormats[video.type]){
+        helpers.submitVideo(video)
+          .then(function(data){
+            helpers.sendCode(data)
+            .then(function(response){
+              this.refs.line.value = '';
+              this.refs.video.value = '';
+              console.log('submitted video for analysis');
+            }.bind(this));
+          }.bind(this))   
+      } else {
+        alert("This is an invalid video format. Please upload .mp4, .mov, or .avi only.");
+      }
+    } else {
+      console.log('choose a file');
+    }
   },
   handleFile: function() {
-    var video = this.refs.video.files[0];
-    var validVideoFormats = {
-      'video/mp4': true,
-      'video/quicktime': true,
-      'video/avi': true
-    };
-    if (validVideoFormats[video.type]){
-      helpers.submitVideo(video)
-        .then(function(data){
-          helpers.sendCode(data)
-          .then(function(response){
-            console.log('submitted video for analysis');
-          });
-        })   
-    } else {
-      alert("This is an invalid video format. Please upload .mp4, .mov, or .avi only.");
-    }
+    this.refs.line.value = this.refs.video.files[0].name;
   },
   render: function() {
     return (
@@ -34,9 +42,12 @@ var VideoInput = React.createClass({
             <input type="file" ref='video' onChange={this.handleFile} />
           </div>
           <div className="file-path-wrapper">
-            <input className="file-path validate" type="text" />
+            <input className="file-path validate" ref="line"type="text" />
           </div>
         </div>
+        <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+          <i className="material-icons right">send</i>
+        </button>
       </form>
     )
   }
