0f2b2aa464c2b5925819184db854b6d461aa10fc
jason3j <jason3j@gmail.com>
2016-01-27 18:34:02 -0700

    change to config

--- server/config.js
+++ server/config.js
@@ -1,23 +1,22 @@
+var config;
 
-var localConfig = require('./config/localConfig.js')
-
-var config = {
-  local: {
+if(process.env.NODE_ENV!=='production'){
+  var localConfig = require('./config/localConfig.js')
+  config ={
     mode: 'local',
     port: 3000,
     mongoUri:localConfig.mongoURI,
     kairosID:localConfig.kairosID,
     kairosKey:localConfig.kairosKey
-  },
-  production: {
+  };
+}else{
+  config = {
     mode: 'production',
     port: process.env.PORT || 3000,
     mongoUri: process.env.MONGOLAB_URI,
     kairosID: process.env.kairosID,
     kairosKey: process.env.kairosKey
-  }
-};
+  };
+}
 
-module.exports = function (mode) {
-  return process.env.NODE_ENV === 'production' ? config.production : config.local;
-}();
+module.exports = config;
