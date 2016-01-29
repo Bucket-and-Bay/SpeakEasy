709122fe6d7620f4f22cf421f0e1e467b346f076
tiff8433 <tiffany.l.huang@gmail.com>
2016-01-29 12:43:01 -0800

    Obtain verbal analysis results

--- server/controllers/audio.controller.js
+++ server/controllers/audio.controller.js
@@ -3,25 +3,22 @@ var ffmpeg = require('fluent-ffmpeg');
 var path = require('path');
 var fs = require('fs');
 var watson = require('watson-developer-cloud');
+var apiKeys = '../config/localConfig.js';
 
-var speechToText = watson.speech_to_text({
-  username: "e2d415ae-2afe-433d-9964-9c68addb8056",
-  password: "1o7EBDH1sxxB",
-  url: "https://stream.watsonplatform.net/speech-to-text/api"
-});
+var speechToText = watson.speech_to_text(apiKeys.watsonCredentials);
 
 /*
-getAudio(videoURL)
+extractAudio(videoURL)
   .then(watsonSpeechToText, function() { console.log('rejected') });
   */
 
-module.exports.getAudio = function(videoURL) {
+module.exports.extractAudio = function(videoURL) {
   return new Promise(function(resolve, reject){
     var wavFile = path.join(__dirname + '/wavFiles/' + 'file.wav');
     ffmpeg(videoURL)
       .output(wavFile)
       .on('end', function(){
-        console.log('getAudio resolved');
+        console.log('extractAudio resolved');
         resolve(wavFile);
       })
       .on('error', function(err){
@@ -44,6 +41,9 @@ module.exports.watsonSpeechToText = function(audioFile) {
     var recognizeStream = speechToText.createRecognizeStream(params);
     //pipe in audio
     fs.createReadStream(audioFile).pipe(recognizeStream);
+    //saves text only to file
+    recognizeStream.pipe(fs.createWriteStream(__dirname + '/wavFiles/' + 'transcription.txt'));
+    // to get strings instead of Buffers from `data` events
     recognizeStream.setEncoding('utf8');
     recognizeStream.on('results', function(e){
       if(e.results[0].final) {
@@ -52,19 +52,12 @@ module.exports.watsonSpeechToText = function(audioFile) {
     });
 
     recognizeStream.on('error', function(err) {
-      util.handleError('Error writing to transcript.json: ' + err);
+      console.log('Error writing to transcript.json: ' + err));
     });
 
     recognizeStream.on('connection-close', function() {
-      var transcriptFile = path.join(__dirname + '/wavFiles/' + 'transcript.json');
-
-      fs.writeFile(transcriptFile, JSON.stringify(results), function(err) {
-        if (err) {
-          util.handleError(err);
-        }
-        //TODO: get text from transcriptFile and pass as parameter to resolve
-        resolve(text);
-      });
+      var verbalAnalysis = JSON.stringify(results);
+      resolve(verbalAnalysis);
     });
   });
 };
