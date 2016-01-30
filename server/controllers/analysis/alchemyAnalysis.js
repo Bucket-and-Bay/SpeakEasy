4be2edce91f8962c5deb08672012abe0e3346631
tiff8433 <tiffany.l.huang@gmail.com>
2016-01-29 18:12:23 -0800

    Setup alchemyapi instance

--- server/controllers/analysis/alchemyAnalysis.js
+++ server/controllers/analysis/alchemyAnalysis.js
@@ -1,21 +1,11 @@
 var apiKeys = require('../../config.js');
 var request = require('request-promise');
+var AlchemyAPI = require('../../alchemyapi.js');
+
+var alchemyapi = newAlchemyAPI();
 
 module.exports.alchemyAnalysis = function (transcript) {
-  var options={
-    method    : 'POST',
-    text      : transcript,
-    url       : 'http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment',
-    apikey    : process.env.KAIROS_ID || apiKeys.alchemyKey,
-    outputMode: json
-  };
-  request(options)
-  .then(function(res,err){
-    if (err){
-      console.log('ERROR',err);
-    }else{
-      var alchemyResults = res;
-      console.log(res)
-    }
+  alchemyapi.sentiment('text', transcript, {}, function(response){
+    console.log("Sentiment: " + response["docSentiment"]["type"]);
   });
 }
