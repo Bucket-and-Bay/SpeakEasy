6b36e122ffd1ac07578c82ab68344d97c2a5412d
tiff8433 <tiffany.l.huang@gmail.com>
2016-01-29 16:14:11 -0800

    Add watsonAnalysis for speech to text

---
+++ server/controllers/analysis/watsonAnalysis.js
@@ -0,0 +1,50 @@
+var Promise = require('bluebird');
+var fs = require('fs');
+var watson = require('watson-developer-cloud');
+var apiKeys = '../../config/localConfig.js';
+
+var speechToText = watson.speech_to_text(apiKeys.watsonCredentials);
+
+var getText = function(data) {
+  var results = [];
+  data.forEach(function(item) {
+    results.push(item['results'][0]['alternatives'][0]['transcript']);
+  });
+  return results.join('');
+};
+
+module.exports.watsonSpeechToText = function(audioFile) {
+  return new Promise(function(resolve, reject){
+    var text = [];
+    var results = [];
+    var params = {
+      content_type: 'audio/wav',
+      timestamps: true,
+      continuous: true
+    };
+    
+    //create stream
+    var recognizeStream = speechToText.createRecognizeStream(params);
+    //pipe in audio
+    fs.createReadStream(audioFile).pipe(recognizeStream);
+    // to get strings instead of Buffers from `data` events
+    recognizeStream.setEncoding('utf8');
+    recognizeStream.on('results', function(e){
+      if(e.results[0].final) {
+        results.push(e);
+      }
+    });
+
+    recognizeStream.on('error', function(err) {
+      console.log('Error writing to transcript.json: ' + err));
+    });
+
+    recognizeStream.on('connection-close', function() {
+      var watsonResults = JSON.stringify(results);
+      var transcript = getText(watsonResults);
+      //TODO: save watsonResults to db
+      resolve(transcript);
+    });
+  });
+};
+
--- server/controllers/audio.controller.js
+++ server/controllers/audio.controller.js
@@ -1,16 +1,19 @@
 var Promise = require('bluebird');
 var ffmpeg = require('fluent-ffmpeg');
 var path = require('path');
-var fs = require('fs');
-var watson = require('watson-developer-cloud');
-var apiKeys = '../config/localConfig.js';
+var watsonAnalysis = require('../config/localConfig.js');
 
-var speechToText = watson.speech_to_text(apiKeys.watsonCredentials);
 
-/*
 extractAudio(videoURL)
-  .then(watsonSpeechToText, function() { console.log('rejected') });
-  */
+  .then(function(wavFile){
+    beyondVerbal(wavFile);              //save beyondVerbalResults to db
+    watsonAnalysis.watsonSpeechToText(wavFile)         //save watsonResults to db
+      .then(function(watsonResults){
+        alchemyAnalysis(watsonResults); //save alchemyResults to db
+      })
+  });
+
+  
 
 module.exports.extractAudio = function(videoURL) {
   return new Promise(function(resolve, reject){
@@ -28,39 +31,7 @@ module.exports.extractAudio = function(videoURL) {
   })
 };
 
-module.exports.watsonSpeechToText = function(audioFile) {
-  return new Promise(function(resolve, reject){
-    var results = [];
-    var params = {
-      content_type: 'audio/wav',
-      timestamps: true,
-      continuous: true
-    };
-    
-    //create stream
-    var recognizeStream = speechToText.createRecognizeStream(params);
-    //pipe in audio
-    fs.createReadStream(audioFile).pipe(recognizeStream);
-    //saves text only to file
-    recognizeStream.pipe(fs.createWriteStream(__dirname + '/wavFiles/' + 'transcription.txt'));
-    // to get strings instead of Buffers from `data` events
-    recognizeStream.setEncoding('utf8');
-    recognizeStream.on('results', function(e){
-      if(e.results[0].final) {
-        results.push(e);
-      }
-    });
 
-    recognizeStream.on('error', function(err) {
-      console.log('Error writing to transcript.json: ' + err));
-    });
-
-    recognizeStream.on('connection-close', function() {
-      var verbalAnalysis = JSON.stringify(results);
-      resolve(verbalAnalysis);
-    });
-  });
-};
 
 
 
