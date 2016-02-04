eceb447864c3f56dcbd1985f5da26c6bb68ffa51
tiff8433 <tiffany.l.huang@gmail.com>
2016-02-03 22:15:17 -0800

    Add public videos component

---
+++ client/components/PublicVideos.js
@@ -0,0 +1,21 @@
+// var React = require('react');
+// var Navbar = require('./Navbar.js');
+
+// var PublicVideos = React.createClass({
+
+// });
+
+// module.exports = VideoInput;
+
+
+
+
+/*
+  Add to server routes.js and on navbar
+  Add client helper function to get all public videos
+  Add server route to get all public videos
+  Add server helper function to query and return all public vids
+  
+
+
+*/
