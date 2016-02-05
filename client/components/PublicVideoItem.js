bb030696635c15c7b8dde663ea88a5df52c04952
tiff8433 <tiffany.l.huang@gmail.com>
2016-02-04 20:53:52 -0800

    Populate public videos page with all videos

---
+++ client/components/PublicVideoItem.js
@@ -0,0 +1,36 @@
+var React = require('react');
+
+var PublicVideoItem = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
+
+  render: function() {
+    var cards = this.props.data.map(function(item, idx) {
+      var date = new Date(item.date).toLocaleDateString() + " " + new Date(item.date).toLocaleTimeString();
+      var location = "#/analysis/" + item._id
+      return  <div className="col s4" key={idx}>
+                <div className="card hoverable small">
+                  <a href={location}>
+                    <div className="card-image">        
+                      <image src={item.thumbnail_url}/>
+                      <span className="card-title">{item.title}</span>
+                    </div>
+                  </a>
+                  <div className="card-content">
+                    <p>Description: {item.description}</p>
+                  </div>
+                  <div id='time'>
+                    Timestamp: {date}
+                  </div>
+                </div>
+              </div> 
+    }.bind(this));
+    return (
+      <div>{cards}</div>
+    )
+  }
+});
+
+module.exports = PublicVideoItem;
+
