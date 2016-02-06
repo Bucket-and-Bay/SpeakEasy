5de734913c563015e56eade86e44401002667994
tiff8433 <tiffany.l.huang@gmail.com>
2016-02-05 19:51:56 -0800

    change variables for production env

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 var config;
-if(process.env.NODE_ENV ==='development'){
+if(process.env.NODE_ENV !=='production'){
+// if(process.env.NODE_ENV ==='development'){
   var localConfig = require('./config/localConfig.js');
   config ={
     mode: 'local',
