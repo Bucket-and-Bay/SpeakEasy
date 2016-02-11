9e2d42cdbcd6f437c159dafa159876c9209130ff
Zan Rosenthal <zan.rosenthal@gmail.com>
2016-02-10 23:44:09 -0500

    Adds parallax to index banner on landing page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var Parallax = require('react-parallax');
-
+var React = require('react');
+var Parallax = require('react-parallax');
 var Home = React.createClass({
   componentDidMount() {
     window.analytics.page('Home Page');
@@ -8,34 +9,38 @@ var Home = React.createClass({
   
   render: function(){
     return ( 
-      <div id="home-container"> 
+      <div> 
       <div id="index-banner" className="parallax-container"> 
-        <div className="section no-pad-bot">
-          <div id="hero-container" className="container center">
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <h1 id="hero-text" className="header center white-text text-lighten-2">Learn to give motivating speeches </h1>
-            <div className="row center">
-              <h5 className="header col s12 light">Improve your public speaking dramatically</h5>
-            </div>
-            <div className="row center">
-              <a href="#/signin" id="download-button" className="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+        <Parallax bgImage="main_1200.jpg" strength={400}>
+          <br/>
+          <div className="section no-pad-bot">
+            <div id="hero-container" className="container">
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <h1 id="hero-text" className="header center white-text text-lighten-2">Learn to give motivating speeches </h1>
+              <div className="row center">
+                <h5 className="header col s12 light">Improve your public speaking dramatically</h5>
+              </div>
+              <div className="row center">
+                <a href="#/signin" id="download-button" className="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+              </div>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
             </div>
           </div>
-        </div>
-        <div className="parallax"><img src="main_1200.jpg" alt="Unsplashed background img 1"/></div>
+        </Parallax>
       </div>
-
       <div className="container">
         <div className="section">
-          <div className="row">
+          <div className="row yellow lighten-5">
             <div className="col s12 m4">
               <div className="icon-block">
                 <h2 className="center teal-text"><i className="material-icons">videocam</i></h2>
                 <h5 className="center">Easy to Use</h5>
-
                 <p className="light">Submit a video recording of yourself or take one on the spot. Just upload the videos you want and we'll send you an email when it's done.</p>
               </div>
             </div>
@@ -43,27 +48,94 @@ var Home = React.createClass({
               <div className="icon-block">
                 <h2 className="center teal-text"><i className="material-icons">group</i></h2>
                 <h5 className="center">Community Driven</h5>
-
-                <p className="light">Recieve comments and tips from our community to help you improve! Please no internet trolls, you will be ban hammered</p>
+                <p className="light">Receive comments and tips from our community to help you improve! Please no internet trolls, you will be ban hammered</p>
               </div>
             </div>
-
             <div className="col s12 m4">
               <div className="icon-block">
                 <h2 className="center teal-text"><i className="material-icons">speaker_notes</i></h2>
                 <h5 className="center">Speech Analysis</h5>
-
                 <p className="light">Get analysis on the emotions you portray when speaking as well as the contents of your speech. Speech and tone analyzation on content of your speechs to help you give positive upbeat</p>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="parallax-container ">
         <Parallax bgImage="combinevideoverbal.jpg" bgHeight="auto" bgWidth="auto" strength={300}>
+          <br/>
+          <div className="section no-pad-bot">
+            <div id="hero-container" className="container">
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+            </div>
+          </div>
         </Parallax>
+      </div>
+      <div className="container">
+        <div className="section">
+          <div className="row">
+            <div className="col s12 m4">
+              <div className="icon-block">
+                <h2 className="center teal-text"><i className="material-icons">videocam</i></h2>
+                <h5 className="center">Easy to Use</h5>
+                <p className="light">Submit a video recording of yourself or take one on the spot. Just upload the videos you want and we'll send you an email when it's done.</p>
+              </div>
+            </div>
+            <div className="col s12 m4">
+              <div className="icon-block">
+                <h2 className="center teal-text"><i className="material-icons">group</i></h2>
+                <h5 className="center">Community Driven</h5>
+                <p className="light">Receive comments and tips from our community to help you improve! Please no internet trolls, you will be ban hammered</p>
+              </div>
+            </div>
+            <div className="col s12 m4">
+              <div className="icon-block">
+                <h2 className="center teal-text"><i className="material-icons">speaker_notes</i></h2>
+                <h5 className="center">Speech Analysis</h5>
+                <p className="light">Get analysis on the emotions you portray when speaking as well as the contents of your speech. Speech and tone analyzation on content of your speechs to help you give positive upbeat</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="parallax-container ">
+        <Parallax bgImage="combinevideoverbal.jpg" bgHeight="auto" bgWidth="auto" strength={400}>
+          <br/>
+          <div className="section no-pad-bot">
+            <div id="hero-container" className="container">
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+            </div>
+            <p>&nbsp;</p>
+          </div>
+        </Parallax>
+      </div>
     </div>
     )
   }
 })
-
 module.exports = Home;
